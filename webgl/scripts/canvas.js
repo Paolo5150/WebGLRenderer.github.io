e5339e50288ae8eb2fe8503e48301b3cac52f61f
Paolo <p.ferri1986@gmail.com>
2024-07-08 19:32:30 +0800

    trying to load bg

--- webgl/scripts/canvas.js
+++ webgl/scripts/canvas.js
@@ -103,7 +103,7 @@ woodMat.addTexture("shadowMap", directionalLight.shadowFrameBuffer.attachments['
 woodMat.addMat4Uniform("lightSpace", ()=>{return directionalLight.ligthtSpaceMatrix  })
 woodMat.addCubeMap("pShadowMap", pointlLight.shadowFrameBuffer.attachments['depth'])
 
-Texture.FromURL_HDR('https://jmp.sh/s/ku0KQity3mAE76vNb2Ew', (loadedTexture)=>{
+Texture.FromURL_HDR('http://myimgbgnd.atwebpages.com/Alexs_Apt_2k.hdr', (loadedTexture)=>{
   
 
   pbrTools.renderToCubemap(renderer, 0, equirectCube, loadedTexture)
